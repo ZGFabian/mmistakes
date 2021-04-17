d0f8efb2fc769be96c68193c36d1cb560896ea19
zgfabian <2885877+ZGFabian@users.noreply.github.com>
2021-04-17 04:35:19 +0200

    xaringan favicon

--- _drafts/x-deploy.R
+++ _drafts/x-deploy.R
@@ -1,2 +1,8 @@
-knitr::knit(input = "xaringan.Rmd")
-rmarkdown::render('xaringan.Rmd','xaringan::moon_reader')
+setwd("~/projects/2020/jekyll/mmistakes/_drafts/")
+# knitr::knit(input = "xaringan.Rmd")
+rmarkdown::render(input = 'xaringan.Rmd',
+                  output_format = 'xaringan::moon_reader',
+                  output_file = "xaringan-test.html",
+                  output_dir = "../static/")
+
+# list.dirs()
