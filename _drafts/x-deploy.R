b92539cdeec66d54e9de1f893c7f17bb0d8b34d4
zgfabian <2885877+ZGFabian@users.noreply.github.com>
2021-04-17 15:47:29 +0200

    xaringan themes and update

--- _drafts/x-deploy.R
+++ _drafts/x-deploy.R
@@ -1,8 +1,12 @@
 setwd("~/projects/2020/jekyll/mmistakes/_drafts/")
-# knitr::knit(input = "xaringan.Rmd")
+
 rmarkdown::render(input = 'xaringan.Rmd',
                   output_format = 'xaringan::moon_reader',
                   output_file = "xaringan-test.html",
                   output_dir = "../static/")
+
+
+names(xaringan:::list_css())
+
 
 # list.dirs()
